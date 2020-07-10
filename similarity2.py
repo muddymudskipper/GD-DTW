be0b9ec8bc0783714a5699a414ec3584d6d33f71
muddymudskipper <thomas.wilmering@gmail.com>
2020-07-10 15:52:12 +0100

    use python vamp host, do everything in memory

--- similarity2.py
+++ similarity2.py
@@ -16,6 +16,7 @@ from subprocess import Popen, DEVNULL, PIPE
 import numpy as np
 from test_subdtw_NEW_tuning import dtwstart
 from uuid import uuid4
+import librosa
 #import pickle
 
 SR = 22050
@@ -24,7 +25,9 @@ RECSDIR = '/Volumes/Beratight2/SDTW/82-10-10'
 
 CPUS = 24
 THREADS_SIMILARITY = 24
-THREADS_DTW = 7
+THREADS_DTW = 6
+
+DTWFRAMESIZE = 512
 
 
 class gl():
@@ -35,11 +38,12 @@ def loadRecordings():
     print('loading audio files')
     recordings = []
     folders = [os.path.join(RECSDIR, d) for d in os.listdir(RECSDIR) if os.path.isdir(os.path.join(RECSDIR, d))]
-    for d in folders:
+    for i, d in enumerate(folders[:2]):
         content = []
         files = [os.path.join(d, f) for f in os.listdir(d) if f.lower().endswith(('flac', 'mp3'))]
         pool = mp.Pool(24)
-        p = pool.map(loadFiles, files, chunksize=1)
+        #p = pool.starmap(loadFiles, zip(files[:3], itertools.repeat(i)), chunksize=1)
+        p = pool.map(loadFiles, files[:3], chunksize=1)
         pool.close()
         pool.join()
         p.sort()
@@ -50,59 +54,63 @@ def loadRecordings():
         etree_number = etreeNumber(rec[0][0])
         for j, f in enumerate(rec):
             #print(i, f[0])
-            gl.shms.append(shared_memory.SharedMemory(create=True, size=f[1].nbytes, name='{0}_{1}'.format(etree_number, j)))
+            gl.shms.append(shared_memory.SharedMemory(create=True, size=f[1].nbytes, name='{0}_{1}_audio'.format(etree_number, j)))
             s = np.ndarray(f[1].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
             s[:] = f[1][:]
 
-            #hpc = hpcpgram(f[1], sampleRate=SR)
-            #gl.shms.append(shared_memory.SharedMemory(create=True, size=hpc.nbytes, name='{0}_{1}'.format(etree_number, j)))
-            #s = np.ndarray(hpc.shape, dtype=np.float32, buffer=gl.shms[-1].buf)
-            #s[:] = hpc[:]
+            gl.shms.append(shared_memory.SharedMemory(create=True, size=f[2].nbytes, name='{0}_{1}_hpcg'.format(etree_number, j)))
+            s = np.ndarray(f[2].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
+            s[:] = f[2][:]
 
-            recordings[i][j] = ('/'.join(f[0].split('/')[-2:]), j, f[1].shape, f[2]) # store only filenames
+            #if i > 0:
+            #    gl.shms.append(shared_memory.SharedMemory(create=True, size=f[3].nbytes, name='{0}_{1}_chroma'.format(etree_number, j)))
+            #    s = np.ndarray(f[3].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
+            #    s[:] = f[3][:]
+            #    recordings[i][j] = ('/'.join(f[0].split('/')[-2:]), j, f[1].shape, f[2].shape, f[3].shape)
+
+            #else:
+            #    recordings[i][j] = ('/'.join(f[0].split('/')[-2:]), j, f[1].shape, f[2].shape)
+            recordings[i][j] = ('/'.join(f[0].split('/')[-2:]), j, f[1].shape, f[2].shape)
+            # 0: filename
+            # 1: index
+            # 2: audio shape
+            # 3: hpcg shape
+            # (4: chroma shape)  # not first (shortest) because it will always be resampled in subdtw script
+
     return recordings
-    
+
+#def loadFiles(f, i):
 def loadFiles(f):
     fs = estd.MonoLoader(filename=f, sampleRate=SR)()
     hpc = hpcpgram(fs, sampleRate=SR)
+    #if i > 0:
+    #    chroma = librosa.feature.chroma_cens(y=fs, sr=SR, hop_length=DTWFRAMESIZE, win_len_smooth=21)
+    #    print(f)
+    #    return (f, fs, hpc, chroma)
+    #else:
+    #    print(f)
+    #    return (f, fs, hpc)
     print(f)
-    return (f, hpc, len(fs))
-
+    return (f, fs, hpc)
+    
+    
 def combinedLength(x):
     l = 0
-    for f in x: l += len(f[1])
+    for f in x: l += len(f[2])
     return l
-
-
-def audioPairs(recs):
-    rec1 = recs[0]
-    rec2 = recs[1]
-    files1 = [os.path.join(rec1, f) for f in os.listdir(rec1) if f.lower().endswith(('flac', 'mp3'))]
-    files2 = [os.path.join(rec2, f) for f in os.listdir(rec2) if f.lower().endswith(('flac', 'mp3'))]
-    if len(files2) > len(files1):
-        temp = files1
-        files1 = files2
-        files2 = temp
-    pairs = list(itertools.product(files1, files2))
-    return pairs
 
 
 def similarity(audiopair):
     #load audio from shared memory
     f1 = audiopair[0][0]
     f2 = audiopair[1][0]
-    shmname1 = '{0}_{1}'.format(etreeNumber(audiopair[0][0]), audiopair[0][1])
-    shmname2 = '{0}_{1}'.format(etreeNumber(audiopair[1][0]), audiopair[1][1])
+    shmname1 = '{0}_{1}_hpcg'.format(etreeNumber(f1), audiopair[0][1])
+    shmname2 = '{0}_{1}_hpcg'.format(etreeNumber(f2), audiopair[1][1])
     shm1 = shared_memory.SharedMemory(name=shmname1)
     shm2 = shared_memory.SharedMemory(name=shmname2)
 
-    #file1 = np.ndarray(audiopair[0][2], dtype=np.float32, buffer=shm1.buf)
-    #file2 = np.ndarray(audiopair[1][2], dtype=np.float32, buffer=shm2.buf)
-    #file1_hpcp = hpcpgram(file1, sampleRate=SR)
-    #file2_hpcp = hpcpgram(file2, sampleRate=SR)
-
-    file1_hpcp = np.ndarray(audiopair[0][2], dtype=np.float32, buffer=shm1.buf)
-    file2_hpcp = np.ndarray(audiopair[1][2], dtype=np.float32, buffer=shm2.buf)
+    file1_hpcp = np.ndarray(audiopair[0][3], dtype=np.float32, buffer=shm1.buf)
+    file2_hpcp = np.ndarray(audiopair[1][3], dtype=np.float32, buffer=shm2.buf)
 
     # Compute binary chroma cross similarity
     crp = estd.ChromaCrossSimilarity(frameStackSize=9,
@@ -118,8 +126,9 @@ def similarity(audiopair):
     f1s = ('/').join(f1.split('/')[-2:])
     f2s = ('/').join(f2.split('/')[-2:])
     print(distance, f1s, f2s)
-    return([f1, f2, distance])
 
+    #return([f1, f2, distance])
+    return([audiopair[0], audiopair[1], distance])
 
 def processResult(p):
     pdict = {}
@@ -128,42 +137,26 @@ def processResult(p):
         if i[0] not in pdict: pdict[i[0]] = []
         pdict[i[0]].append([i[2], i[1]])
     for k, v in pdict.items():
-        smin = sorted(v)[:2]        # store first 2 min distances
-        #smin = sorted(v)[:1]        # for testing
-        for i in smin: res.append([k] + i)
+        #smin = sorted(v)[:2]        # store first 2 min distances
+        smin = sorted(v)[:1]        # for testing
+        for i in smin: res.append([k] + [i[1]]) # store recording pairs with all info
+    print(res)
     return res
 
 
 def runScript(f):
-    file1 = f[0]
-    file2 = f[2]
+    file1 = f[0][0]
+    file2 = f[1][0]
     print(file1, file2)
-    resfile = dtwstart(os.path.join(RECSDIR, file1), os.path.join(RECSDIR, file2))
+    #resfile = dtwstart(os.path.join(RECSDIR, file1), os.path.join(RECSDIR, file2))
+
+    resfile = dtwstart(f[0], f[1], RECSDIR)
     return resfile
 
 
 def start():
     filenames = loadRecordings()
-    # compare to longest
-    '''
-    audiopairs = []
-    matched_files = []
-
-    for n in range(0, len(filenames)-1):
-        apairs = list(itertools.product(filenames[n], filenames[-1]))
-        audiopairs += apairs
-    matched_files = process(audiopairs)
-
-    audiopairs = []
-
-    
-    for n in range(0, len(filenames)-2):
-        apairs = list(itertools.product(filenames[n], filenames[-2]))
-        audiopairs += apairs
-        audiopairs = list(filter(lambda x: x[0][0] not in matched_files, audiopairs))
-        print(audiopairs)
-
-    '''
+    # compare to longest first
 
     matched_files = []
     for i in range(1,len(filenames)):
@@ -177,9 +170,11 @@ def start():
         if len(audiopairs) > 0: matched_files += process(audiopairs)
         else: break
 
+        #break
+
     
 def process(apairs):
-    manager = mp.Manager()
+    #manager = mp.Manager()
     pool = mp.Pool(THREADS_SIMILARITY)
     p = pool.map(similarity, apairs, chunksize=1)
     pool.close()
--- test_subdtw_NEW_tuning.py
+++ test_subdtw_NEW_tuning.py
@@ -8,8 +8,9 @@ from uuid import uuid4
 import soundfile as sf
 import matplotlib.pyplot as plt
 from scipy import stats
-from math import ceil
-
+#from math import ceil
+from multiprocessing import shared_memory
+import vamp
 
 SR = 22050
 DTWFRAMESIZE = 512
@@ -36,7 +37,7 @@ DST = DIR + '2020/results/'
 #FILE1 = None
 #FILE2 = None
 
-
+    
 class gl():
     dstdir = None
 
@@ -79,7 +80,8 @@ def removeNonlinear(wp):
 
 
 def scaleDtw(wp, t):
-    wp[:, 1] *= 2**(-t / 1200)
+    #wp[:, 1] *= 2**(-t / 1200)
+    wp[:, 0] *= 2**(-t / 1200)
     return wp
 
 
@@ -112,6 +114,13 @@ def readAudio(wav, tuning=float(0)):
         a = resampleAudio(a, tuning)
     return a, leng
 
+def resampleAudio2(a, tuning=float(0)):
+    if tuning != float(0): 
+        ratio = 2**(-tuning / 1200) # -tuning because resampling of audio 1
+        ar = samplerate.resample(a, ratio, 'sinc_best') 
+        return ar
+    else:
+        return a
 
 def resampleAudio(a, t):
     ratio = 2**(t / 1200)
@@ -134,14 +143,28 @@ def tuningFrequency(a, b):
     #    text_file.write(str(diff))
     return float(diff)
 
-#def plotFigure(w, l1, l2):
-#    w = np.array(w)
-#    p = plt.figure()
-#    plt.plot(w[:, 0], w[:, 1], color='y')
-#    plt.plot(0, 0, color='w')  # include full audio length in plot
-#    plt.plot(l1/SR, l2/SR, color='w')
-#    plt.tight_layout()
-#    p.savefig(PNGFILE, bbox_inches='tight')
+
+def tuningFrequency2(a, b):
+    two_channels = makeTwoChannels(a,b) 
+    diff = vamp.collect(two_channels, SR, "tuning-difference:tuning-difference", output="cents", parameters={'maxduration': 300})
+    diff = diff['list'][0]['values'][0]
+    #print('diff = ', float(diff))
+    return float(diff) 
+
+
+def makeTwoChannels(a, b):
+    if len(a) > len(b):
+        pad = np.pad(b, [0,len(a)-len(b)])
+        return np.array([a, pad])
+    elif len(b) > len(a):
+        pad = np.pad(a, [0,len(b)-len(a)])
+        return np.array([pad, b])
+    else:
+        return np.array([a, b])
+
+
+
+
 
 def plotFigure2(ws, l1, l2, file1, file2):
     fsplit1 = file1.split('/')
@@ -175,43 +198,72 @@ def plotFigure2(ws, l1, l2, file1, file2):
     p.savefig(pdfname, bbox_inches='tight')
     plt.close(p)
 
-def makeFolders(f1, f2):
-    fid = [None, None]
-    for i, f in enumerate([f1, f2]):
-        for j in f.split('/')[-2].split(('.')):
-            try:
-                fid[i] = int(j)
-                break
-            except: pass
-    gl.dstdir = os.path.join(DST, '{0}_{1}'.format(fid[0], fid[1]))
+def makeFolders(e1, e2):
+    #fid = [None, None]
+    #for i, f in enumerate([f1, f2]):
+        #for j in f.split('/')[-2].split(('.')):
+        #    try:
+        #        fid[i] = int(j)
+        #        break
+        #    except: pass
+    #    fid[i] = etreeNumber(f)
+
+    gl.dstdir = os.path.join(DST, '{0}_{1}'.format(e1, e2))
     for d in [TEMP, DST, gl.dstdir]:
         if not os.path.exists(d):
             try: os.makedirs(d)
             except: pass
 
 
-def dtwstart(FILE1, FILE2):
+def etreeNumber(e):
+    for j in e.split('/')[-2].split(('.')):
+        try: return int(j)
+        except: pass
+
+
+def dtwstart(FILE1, FILE2, RECSDIR):
+    etree_number1 = etreeNumber(FILE1[0])
+    etree_number2 = etreeNumber(FILE2[0])
+
     resfile = None
-    makeFolders(FILE1, FILE2)
+    makeFolders(etree_number1, etree_number2)
     #return
 
+    shmname1 = '{0}_{1}_audio'.format(etree_number1, FILE1[1])
+    shm1 = shared_memory.SharedMemory(name=shmname1)
+    file1_buf = np.ndarray(FILE1[2], dtype=np.float32, buffer=shm1.buf)
+    shmname2 = '{0}_{1}_audio'.format(etree_number2, FILE2[1])
+    shm2 = shared_memory.SharedMemory(name=shmname2)
+    file2_buf = np.ndarray(FILE2[2], dtype=np.float32, buffer=shm2.buf)
     
-    file1 = monoWav(FILE1)
-    file2 = monoWav(FILE2)
+
+    tuning = tuningFrequency2(file1_buf, file2_buf)
+
+    file1_resampled = resampleAudio2(file1_buf, tuning)
+    X = getChroma(file1_resampled)
+    Y = getChroma(file2_buf)
+
+
+    filename1 = os.path.join(RECSDIR, FILE1[0])
+    filename2 = os.path.join(RECSDIR, FILE2[0])
+    '''
+    file1 = monoWav(filename1)
+    file2 = monoWav(filename2)
     tuning = tuningFrequency(file1, file2)
     file1_np, len1 = readAudio(file1)
     file2_np, len2 = readAudio(file2, tuning)
-
-
     #print(Y.shape)
     X = getChroma(file1_np)
     Y = getChroma(file2_np)
+    '''
+
     wp_plot = libDtw(X, Y, tuning)
     #print(wp_plot)
 
     if len(wp_plot) > 0:
-        plotFigure2(wp_plot, len1, len2, FILE1, FILE2)
-        resfile = '/'.join(FILE1.split('/')[-2:])
+        #plotFigure2(wp_plot, len1, len2, filename1, filename2)
+        plotFigure2(wp_plot, FILE1[2][0], FILE2[2][0], filename1, filename2)
+        resfile = '/'.join(filename1.split('/')[-2:])
         dtw = wp_plot[0]
         #if len(wp_plot) > 1:
         #    for d in wp_plot[1:]: 
