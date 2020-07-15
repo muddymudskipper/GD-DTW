c9bda215b911e9cfdb3f34b48fb6c96951573e61
muddymudskipper <thomas.wilmering@gmail.com>
2020-07-15 20:18:01 +0100

    omit flac1648 and flac24 folders

--- make_folder_dict.py
+++ make_folder_dict.py
@@ -10,13 +10,32 @@ DIRS = [os.path.join(SBDPATH, d) for d in os.listdir(SBDPATH) if os.path.isdir(o
 
 
 datedict = {}
+
+
 for d in DIRS:
-    if 'flac24' in d: continue
-    print(d)
+    #if 'flac24' in d: continue
+    
+
+
     date = d.split('/')[-1].split('.')[0]
     try:
         date = re.findall(r'\d{2}-\d{2}-\d{2}', date)[0]
         if date not in datedict: datedict[date] = []
+        
+        if d.replace('flac24', 'flac16') in datedict[date]:
+            continue
+        elif d.replace('flac1648', 'flac16') in datedict[date]:
+            continue
+        elif d.replace('flac24', 'flac1648') in datedict[date]:
+            continue
+        if d.replace('flac16', 'flac24') in datedict[date]:
+            datedict[date].remove(d)
+        elif d.replace('flac16', 'flac1648') in datedict[date]:
+            datedict[date].remove(d)
+        elif d.replace('flac1648', 'flac24') in datedict[date]:
+            datedict[date].remove(d)
+
+
         datedict[date].append(d)
     except: pass
 
