40366b462880cd83f8397800b0d01fcab66f6dab
muddymudskipper <thomas.wilmering@gmail.com>
2020-07-17 17:11:14 +0100

    init match aligner, renamed files, edits

---
+++ align_match.py
@@ -0,0 +1,476 @@
+# https://essentia.upf.edu/essentia_python_examples.html
+
+# 1. find longest performance
+# 2. try alignemnet of all others with performance from 1.
+# 3. store files with no alignment
+# 4. repeat from 1. with unaligned files and reference next longest recording
+
+
+import essentia.standard as estd
+from essentia.pytools.spectral import hpcpgram
+import itertools, os, json, sys
+import multiprocessing as mp
+from multiprocessing import shared_memory
+from subprocess import Popen, DEVNULL, PIPE
+import numpy as np
+from uuid import uuid4
+from librosa.feature import chroma_cens
+from tqdm import tqdm
+from subprocess import Popen, DEVNULL, PIPE
+import pickle
+from multiprocessing.managers import SharedMemoryManager
+from math import ceil, floor, log2
+from random import shuffle
+import vamp
+
+#from test_subdtw_NEW_tuning import dtwstart
+from match_module import matchStart
+from make_folder_dict import dateDict
+
+#RECSDIR = '/Volumes/Beratight2/SDTW/82-07-29'
+#DIR = '/Volumes/Journal/Documents/OneDrive/OneDrive - Queen Mary, University of London/projects/SDTW/'
+#TEMPDIR = DIR + 'temp/'
+
+TOTAL_MEM = 64
+HEADROOM_MEM = 10
+FREE_MEM = TOTAL_MEM - HEADROOM_MEM
+B_TO_GB = 1 / 2**30
+
+DATE = sys.argv[1]
+#DATE = '82-07-29'
+TEMPDIR = 'temp'
+DSTDIR = os.path.join('results', DATE)
+
+CPUS = 24
+THREADS_LOADING = 24
+#THREADS_CHROMA = 24
+THREADS_SIMILARITY = 12
+#THREADS_DTW = 10
+THREADS_TUNING = 24
+THREADS_MATCH = 24
+
+SR = 22050
+DTWFRAMESIZE = 512
+
+
+
+class gl():
+    shms = []
+
+
+def loadRecordings():
+    print('loading audio files')
+
+    folders = pickle.load(open('date_folder_dict.pickle', 'rb'))[DATE]
+    #folders = [os.path.join(RECSDIR, d) for d in os.listdir(RECSDIR) if os.path.isdir(os.path.join(RECSDIR, d))]
+
+    #recordings = pickle.load(open('recordings.pickle', 'rb'))
+    #folders = dateDict()[DATE]
+
+    recordings = []
+    for d in folders[:2]:
+        print('loading files for', d.split('/')[-1])
+        files = [os.path.join(d, f) for f in os.listdir(d) if f.lower().endswith(('flac', 'mp3', 'shn'))]
+        pool = mp.Pool(THREADS_LOADING)
+        p = list(tqdm(pool.imap(loadFiles, files[:2]), total=len(files[:2])))
+        pool.close()
+        pool.join()
+        p = list(filter(lambda x: x != None, p)) # remove None type for unloadable files
+        p.sort()
+        recordings.append(p)
+    recordings = sorted(recordings, key=lambda x: combinedLength(x))
+    # shared memory for each audio file
+
+    #pickle.dump(recordings, open('recordings.pickle', 'wb'))
+    
+
+    for i, rec in enumerate(recordings):
+        etree_number = etreeNumber(rec[0][0])
+        for j, f in enumerate(rec):
+            #print(i, f[0])
+            gl.shms.append(shared_memory.SharedMemory(create=True, size=f[1].nbytes, name='{0}_{1}_audio'.format(etree_number, j)))
+            s = np.ndarray(f[1].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
+            s[:] = f[1][:]
+
+            gl.shms.append(shared_memory.SharedMemory(create=True, size=f[2].nbytes, name='{0}_{1}_hpcp'.format(etree_number, j)))
+            s = np.ndarray(f[2].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
+            s[:] = f[2][:]
+
+            recordings[i][j] = ('/'.join(f[0].split('/')[-2:]), j, f[1].shape, f[2].shape)
+
+            #recordings[i][j] = (f[0], j, f[1].shape, f[2].shape)
+            # 0: filename
+            # 1: index
+            # 2: audio shape
+            # 3: hpcp shape
+    
+
+    return recordings
+
+
+def loadFiles(f):
+    try:
+        if f.endswith('.shn'):
+            _f = os.path.join(TEMPDIR, str(uuid4()) + '.wav')
+            cmd = 'shorten -x "{0}" "{1}"'.format(f, _f)
+            p = Popen(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL).wait()
+        else: _f = f
+        fs = estd.MonoLoader(filename=_f, sampleRate=SR)()
+        if f.endswith('.shn'): os.remove(_f)
+        hpc = hpcpgram(fs, sampleRate=SR)
+        print(f)
+        return (f, fs, hpc)
+    except: pass
+    
+    
+def combinedLength(x):
+    l = 0
+    for f in x: l += len(f[2])
+    return l
+
+
+def similarity(audiopair):
+    #load audio from shared memory
+    f1 = audiopair[0][0]
+    f2 = audiopair[1][0]
+    shmname1 = '{0}_{1}_hpcp'.format(etreeNumber(f1), audiopair[0][1])
+    shmname2 = '{0}_{1}_hpcp'.format(etreeNumber(f2), audiopair[1][1])
+    shm1 = shared_memory.SharedMemory(name=shmname1)
+    shm2 = shared_memory.SharedMemory(name=shmname2)
+    file1_hpcp = np.ndarray(audiopair[0][3], dtype=np.float32, buffer=shm1.buf)
+    file2_hpcp = np.ndarray(audiopair[1][3], dtype=np.float32, buffer=shm2.buf)
+
+    # Compute binary chroma cross similarity
+    crp = estd.ChromaCrossSimilarity(frameStackSize=9,
+                                    frameStackStride=1,
+                                    binarizePercentile=0.095,
+                                    oti=True)
+    pair_crp = crp(file1_hpcp, file2_hpcp)
+    #   Computing cover song similarity distance
+    score_matrix, distance = estd.CoverSongSimilarity(disOnset=0.5,
+                                                    disExtension=0.5,
+                                                    alignmentType='serra09',
+                                                    distanceType='asymmetric')(pair_crp)
+    #f1s = ('/').join(f1.split('/')[-2:])
+    #f2s = ('/').join(f2.split('/')[-2:])
+    #print(distance, f1s, f2s)
+    
+    return([audiopair[0], audiopair[1], distance])
+
+
+def processResult(p):
+    pdict = {}
+    res = []
+    for i in p:
+        if i[0] not in pdict: pdict[i[0]] = []
+        pdict[i[0]].append([i[2], i[1]])
+    for k, v in pdict.items():
+        smin = sorted(v)[:2]        # store first 2 min distances
+        #smin = sorted(v)[:1]        # for testing
+        for i in smin: res.append([k] + [i[1]]) # store recording pairs with all info
+
+    
+    ## calculate chromas for all audio files that are second input for dtw
+    # match: calculate tuning and tuning difference for all audio files that files are matched against
+    audio_compared_to = []
+    [audio_compared_to.append(f[1]) for f in res]
+    audio_compared_to = list(set(audio_compared_to))
+    tunings = getTunings(audio_compared_to)
+    # append tuning freq
+    for i in res: i.append(tunings[i[1][0]][1])
+    print('getting tuning differences')
+    pool = mp.Pool(THREADS_TUNING)
+    res = list(tqdm(pool.imap(tuningDiffStart, res), total=len(res)))
+    pool.close()
+    pool.join()
+
+    '''
+    chroma_shapes = getChromas(audio_compared_to)
+    # append chroma shape
+    for i in res: i.append(chroma_shapes[i[1][0]][1])
+    #print(res)
+    #print(audio_compared_to)
+
+    res = estimate_memory(res)
+    res = groupPairsBySize(res)
+    '''
+    #print(res[0])
+    return res
+
+
+def getTunings(fs):
+    print('getting tuning frequencies')
+    pool = mp.Pool(THREADS_TUNING)
+    p = list(tqdm(pool.imap(getTuning, fs), total=len(fs)))
+    pool.close()
+    pool.join()
+    p.sort()
+    tunings = {}
+    for c in p:
+        tunings[c[0]] = (c[1], c[2])   # index, tuning
+    return tunings
+
+
+def getTuning(f):
+    shmname = '{0}_{1}_audio'.format(etreeNumber(f[0]), f[1])
+    shm = shared_memory.SharedMemory(name=shmname)
+    a = np.ndarray(f[2], dtype=np.float32, buffer=shm.buf)
+    freq = vamp.collect(a, SR, 'nnls-chroma:tuning', output="tuning")
+    freq = freq['list'][0]['values'][0]
+    return (f[0], f[1], freq)  # return filename, index, tuning
+
+
+def groupPairsBySize(res):
+    # group processes by how many parallel processes can be run
+    shm_mem = sum(s.size for s in gl.shms) * B_TO_GB
+    #print(shm_mem)
+    avail_mem = FREE_MEM - shm_mem
+
+    # sort audiopairs by memory use
+    res = sorted(res, key=lambda x: x[4], reverse=True)
+    total = len(res)
+    resdict = {}
+    for r in res:
+        #cpus = int(avail_mem / r[4])
+        #if cpus == 0: cpus = 1
+        cpus = avail_mem / r[4]
+        if cpus < 1: 
+            cpus = 1
+        else:
+            cpus = normal_round(cpus + (0.2 * cpus))
+            if cpus > CPUS: cpus = CPUS
+        if cpus not in resdict: 
+            resdict[cpus] = []
+        resdict[cpus].append(r)
+
+    reslist = sorted([(k, v) for k, v in resdict.items()])  # [0] = cpus, [1] = audiopairs
+    #json.dump(resdict, open('cpus.json', 'w', encoding='utf-8'), sort_keys=True)
+    for i, r in enumerate(reslist[:-1]):
+        while len(r[1]) < r[0]:
+            moved = False
+            for s in reslist[i+1:]:
+                for a in s[1]:
+                    moved = True
+                    s[1].remove(a)
+                    r[1].append(a) 
+            if not moved: break
+
+    reslist = list(filter(lambda x: len(x[1]) > 0, reslist))
+    [shuffle(i[1]) for i in reslist[1:]]
+
+    #json.dump(reslist, open('cpus3.json', 'w', encoding='utf-8'), sort_keys=True)
+    return reslist
+
+
+def normal_round(n):
+    if n - floor(n) < 0.5:
+        return floor(n)
+    return ceil(n)
+
+
+def runScript(f):
+    #file1 = f[0][0]
+    #file2 = f[1][0]
+    #print(file1, file2)
+    # f[2] = chroma shape of f[1]
+    #resfile = dtwstart(f[0], f[1], f[2], DATE, f[3])
+    resfile = matchStart(f[0], f[1], f[2], DATE, f[3])
+    # file1, file2, tuning, date, tuning diff
+   
+    return resfile
+
+
+def start():
+    filenames = loadRecordings()
+    # compare to longest first
+
+    matched_files = []
+    for i in range(1,len(filenames)):
+        audiopairs = []
+        print('Run {0} of {1}'.format(i, len(filenames)-1))
+        for n in range(0, len(filenames)-i):
+            apairs = list(itertools.product(filenames[n], filenames[-i]))
+            audiopairs += apairs
+            audiopairs = list(filter(lambda x: x[0][0] not in matched_files, audiopairs))
+        #for p in audiopairs: print(p)
+        if len(audiopairs) > 0: matched_files += process(audiopairs, filenames[-i])
+        else: 
+            print('finished')
+            break
+        
+        #break
+
+def unlinkShm(fs, ftype):
+    print('cleaning memory ({0})'.format(ftype))
+    for f in fs:
+        try:                # chroma might not exist for each
+            shmname = '{0}_{1}_{2}'.format(etreeNumber(f[0]), f[1], ftype)
+            #print(shmname)
+            shm = shared_memory.SharedMemory(name=shmname)
+            shm.close()
+            shm.unlink()
+        except: pass
+
+
+
+def getChromas(fs):
+    print('getting chromas')
+    pool = mp.Pool(THREADS_CHROMA)
+    #p = pool.map(getChroma, fs, chunksize=1)
+    p = list(tqdm(pool.imap(getChroma, fs), total=len(fs)))
+    pool.close()
+    pool.join()
+    p.sort()
+    chroma_shapes = {}
+    for c in p:
+        gl.shms.append(shared_memory.SharedMemory(create=True, size=c[2][0].nbytes, name='{0}_{1}_chroma'.format(etreeNumber(c[0]), c[1])))
+        s = np.ndarray(c[2][0].shape, dtype=np.float32, buffer=gl.shms[-1].buf)
+        s[:] = c[2][0][:]   
+        chroma_shapes[c[0]] = (c[1], (c[2][0].shape, c[2][1]))   # c[2][1] = tuning_frac
+    return chroma_shapes
+
+
+def getChroma(f):
+    shmname = '{0}_{1}_audio'.format(etreeNumber(f[0]), f[1])
+    shm = shared_memory.SharedMemory(name=shmname)
+    a = np.ndarray(f[2], dtype=np.float32, buffer=shm.buf)
+    tuning_frac = tuningFreq(a)
+    c = chroma_cens(y=a, sr=SR, hop_length=DTWFRAMESIZE, win_len_smooth=21, tuning=tuning_frac)
+    #print(f[0])
+    return (f[0], f[1], (c, tuning_frac))  # return filename, index, (chroma, tuning_frac)
+
+
+def tuningFreq(b):
+    freq = vamp.collect(b, SR, 'nnls-chroma:tuning', output="tuning")
+    freq = freq['list'][0]['values'][0]
+    frac = 12 * log2(freq / 440)
+    return frac 
+
+
+def tuningDiffStart(fp):
+    #fp.append(0)
+    #return fp
+    file1 = fp[0]
+    file2 = fp[1]
+    etree_number1 = etreeNumber(file1[0])
+    etree_number2 = etreeNumber(file2[0])
+    shmname1 = '{0}_{1}_audio'.format(etree_number1, file1[1])
+    shm1 = shared_memory.SharedMemory(name=shmname1)
+    file1_buf = np.ndarray(file1[2], dtype=np.float32, buffer=shm1.buf)
+    shmname2 = '{0}_{1}_audio'.format(etree_number2, file2[1])
+    shm2 = shared_memory.SharedMemory(name=shmname2)
+    file2_buf = np.ndarray(file2[2], dtype=np.float32, buffer=shm2.buf)
+    tuning_diff = tuningDiff(file1_buf, file2_buf)
+    fp.append(tuning_diff)
+    return fp
+
+
+def tuningDiff(a, b):
+    two_channels = makeTwoChannels(a,b) 
+    diff = vamp.collect(two_channels, SR, "tuning-difference:tuning-difference", output="cents", parameters={'maxrange': 4})
+    diff = diff['list'][0]['values'][0]
+    if diff > 300: diff = 0     # if more than 3 semitones difference there might be something wrong
+    #print('diff = ', float(diff))
+    return float(diff) 
+
+
+def makeTwoChannels(a, b):
+    if len(a) > len(b):
+        pad = np.pad(b, [0,len(a)-len(b)])
+        return np.array([a, pad])
+    elif len(b) > len(a):
+        pad = np.pad(a, [0,len(b)-len(a)])
+        return np.array([pad, b])
+    else:
+        return np.array([a, b])
+
+
+def process(apairs, filenames2):
+    print('measuring pairwise similarity')
+    
+    pool = mp.Pool(THREADS_SIMILARITY)
+    p = list(tqdm(pool.imap(similarity, apairs), total=len(apairs)))
+    pool.close()
+    pool.join()
+    
+    #p = pickle.load(open('similaritymin_test.pickle', 'rb'))
+    #pickle.dump(p, open('similaritymin_test.pickle', 'wb'))
+    
+    res = processResult(p)
+    print('calculating match alignment')
+    pool = mp.Pool(THREADS_MATCH)
+    p = list(tqdm(pool.imap(runScript, res), total=len(res)))
+    pool.close()
+    pool.join()
+    
+    '''
+    total = sum([len(i[1]) for i in res])
+    count = 0
+    p = []
+    for i, e in enumerate(res):
+        le = len(e[1])
+        print(f'{i+1}/{len(res)} ({e[0]} CPUs) [total: {count}/{total}]')
+        threads = e[0]
+        if threads > le: threads = le
+        pool = mp.Pool(threads)
+        q = list(tqdm(pool.imap(runScript, e[1]), total=le))
+        pool.close()
+        pool.join()
+        p += q
+        count += le
+    '''
+    unlinkShm(filenames2, 'audio')
+    #unlinkShm(filenames2, 'chroma')
+    
+    matched_files = list(set(filter(lambda x: x != None, p)))
+    #print(matched_files)
+    return matched_files
+    
+
+def estimate_memory(res):
+    mem_estims = []
+    print('getting tuning differences')
+    pool = mp.Pool(THREADS_TUNING)
+    p = list(tqdm(pool.imap(tuningDiffStart, res), total=len(res)))
+    pool.close()
+    pool.join()
+
+    for r in p:
+        file_len1 = r[0][2][0]
+        ratio = 2**(-r[3] / 1200)
+        file_len1 *= ratio
+    
+        chroma_len1 = ceil(ceil(file_len1) / DTWFRAMESIZE)
+        chroma_len2 = r[2][0][1]
+        msize = 3 * chroma_len1 * chroma_len2
+        psize = min([chroma_len1, chroma_len2]) * 2
+        res = (msize + psize) * 8 * B_TO_GB
+        r.append(res)
+    
+    return p
+
+
+def etreeNumber(e):
+    for j in e.split('/')[-2].split(('.')):
+        try: return int(j)
+        except: pass
+
+
+def remove_empty_folders():
+    folders = list(os.walk(DSTDIR))[1:]
+    for folder in folders:
+        if not folder[2]: os.rmdir(folder[0])
+
+
+if __name__ == '__main__':
+    #os.system('ulimit -n 30000')
+    start()
+    
+    for s in gl.shms:   # there shouldn't be any open ones, but just in case
+        try:
+            s.close()
+            s.unlink()
+        except: pass
+    remove_empty_folders()
+    #os.system('stty sane')
+
---
+++ match_module.py
@@ -0,0 +1,217 @@
+#!/opt/local/bin/python
+# test sub dtw post AES
+
+import librosa, os, json, sys, warnings
+from samplerate import resample
+import numpy as np
+from subprocess import Popen, DEVNULL, PIPE
+from uuid import uuid4
+import matplotlib.pyplot as plt
+from scipy import stats
+from math import ceil
+from multiprocessing import shared_memory
+import vamp
+
+
+np.seterr(divide='ignore', invalid='ignore')   # stats division by zero warning
+
+
+SR = 22050
+DTWFRAMESIZE = 512
+SEGMENT_LENGTH = 10  # length for wp segments in linear regression 
+LINREGRESS_MIN = 0.94
+LINREGRESS_MAX = 1.04
+
+MATCH_INCREMENT = 0.02
+
+#DIR = '/Volumes/Journal/Documents/OneDrive/OneDrive - Queen Mary, University of London/projects/SDTW/'
+#TEMP = DIR + 'temp/'
+#DST = DIR + '2020/results/'
+TEMP = 'temp'
+DST = 'results'
+
+
+#FILE1 = sys.argv[1]
+#FILE2 = sys.argv[2]
+#FILE1 = '/Volumes/Beratight2/SDTW/82-10-10/gd1982-10-10.nak700.anon-poris.LMPP.95682.flac16/gd82-10-10d3t06.flac'
+#FILE2 = '/Volumes/Beratight2/SDTW/82-10-10/gd1982-10-10.nak700.wagner.miller.109822.flac16/gd82-10-10d3t06.flac'
+
+#FILE1 = '/Volumes/Beratight2/SDTW/test/gd1971-08-06d2t04_part.flac'
+#FILE2 = '/Volumes/Beratight2/SDTW/test/gd1971-08-06d2t04_part.flac'
+#FILE1 = None
+#FILE2 = None
+
+
+    
+class gl():
+    dstdir = None
+
+
+def libDtw(X, Y, tuning_diff=0):
+    #sigma = np.array([[1, 1], [1, 2], [2, 1]])
+    D, wp = librosa.sequence.dtw(X, Y, subseq=True, global_constraints=True)
+    wp = processPath(wp, tuning_diff)
+    #with open("warping_path.txt", "w") as text_file:
+    #    for w in wp: text_file.write(str(w) + '\n')
+    return wp
+
+
+def match(X, Y, tuning=440, tuning_diff=0):
+    tuning = float(tuning)
+    #print(tuning)
+    chns, swap = makeTwoChannels(X, Y)
+    p = vamp.collect(chns, SR, 'match-vamp-plugin:match-subsequence', output="path", parameters={'freq1': tuning, 'freq2': tuning})
+    wp = []
+    for i in p['list']:
+        if swap:
+            wp.append([float(i['values'][0]), float(i['timestamp'])])
+        else:
+            wp.append([float(i['timestamp']), float(i['values'][0])])
+    wp = processPath(np.array(wp), tuning_diff)
+    #print(wp)
+    return wp
+
+    
+    
+def makeTwoChannels(a, b):
+    if len(a) > len(b):
+        pad = np.pad(b, [0,len(a)-len(b)])
+        return np.array([a, pad]), False
+    elif len(b) > len(a):
+        pad = np.pad(a, [0,len(b)-len(a)])
+        return np.array([b, pad]), True
+    else:
+        return np.array([a, b]), False
+    
+
+def processPath(wp, tuning_diff):
+    #wp = wp / SR * DTWFRAMESIZE            # not needed for match
+    if tuning_diff != float(0): wp = scaleDtw(wp, tuning_diff)
+    wp = wp[wp[:,0].argsort()]
+    wp = removeNonlinear(wp)
+    #wp = wp.tolist()
+    return wp
+
+
+def removeNonlinear(wp):
+    #slen = SEGMENT_LENGTH * SR / DTWFRAMESIZE
+    slen = SEGMENT_LENGTH / MATCH_INCREMENT
+
+    number_of_chunks = len(wp) / slen
+    chunks = np.array_split(wp, number_of_chunks)  # split to chunks of roughly same length
+    wp_plot = []
+    for chunk in chunks:
+        slope, intercept, r_value = stats.linregress(chunk)[:3]
+        if LINREGRESS_MAX > round(slope, 2) > LINREGRESS_MIN:
+            wp_plot.append(chunk)
+    if len(wp_plot) == 1: wp_plot = []  # testing: omit if only one chunk is aligned to avoid false positives
+    return wp_plot
+
+
+def _removeNonlinear(wp):   # testing
+    frames_per_second = int(SR / DTWFRAMESIZE)
+    len_ceil_seconds = ceil(len(wp) / frames_per_second)
+    wp_plot = []
+    # next chunks with 1s shifting window of 10s:
+    for n in range(0, len_ceil_seconds):
+         chunk = wp[n*frames_per_second:(n+10)*frames_per_second]
+         slope, intercept, r_value = stats.linregress(chunk)[:3]
+         if 1.04 > round(slope, 2) > 0.96:
+             if len(wp_plot) == 0: wp_plot.append(chunk)
+             else: wp_plot.append(chunk[-frames_per_second:])
+    if len(wp_plot) == 1: wp_plot = []  # testing: omit if only one chunk is aligned to avoid false positives
+    return wp_plot
+
+
+def scaleDtw(wp, tuning_diff):
+    wp[:, 0] *= 2**(tuning_diff / 1200)
+    return wp
+
+
+def resampleAudio(a, tuning_diff=0):
+    if tuning_diff != 0: 
+        ratio = 2**(-tuning_diff / 1200) # -tuning because resampling of audio 1
+        #ar = resample(a, ratio, 'sinc_best')
+        #ar = resample(a, ratio, 'sinc_medium')
+        ar = resample(a, ratio, 'sinc_fastest')
+        return ar
+    else:
+        return a
+
+
+def plotFigure(ws, l1, l2, file1, file2, tuning_diff):
+    fsplit1 = file1.split('/')
+    #fname1 = '/'.join(fsplit1[-2:])
+    fsplit2 = file2.split('/')
+    #fname2 = '/'.join(fsplit2[-2:])
+    pdfname = os.path.join(gl.dstdir, '{0}_{1}.png'.format(fsplit1[-1], fsplit2[-1]))
+
+    jsonname = os.path.join(gl.dstdir, '{0}_{1}.json'.format(fsplit1[-1], fsplit2[-1]))
+    dtw = ws[0]
+    if len(ws) > 1:
+        for d in ws[1:]: 
+            dtw = np.concatenate((dtw, d), axis=0)
+    dtw = dtw.tolist()
+    j = { 'dtw': dtw, 'filenames': [file1, file2], 'lengths': [l1/SR, l2/SR], 'tuning_diff': tuning_diff }
+    json.dump(j, open(jsonname, 'w', encoding='utf-8'), sort_keys=True)
+
+    p = plt.figure()
+    plt.title('{0}\n{1}'.format(file1, file2))
+    for w in ws:
+        plt.plot(w[:, 0], w[:, 1], color='y')
+    plt.plot(0, 0, color='w')  # include full audio length in plot
+    plt.plot(l1/SR, l2/SR, color='w')
+    plt.tight_layout()
+    p.savefig(pdfname, bbox_inches='tight')
+    plt.close(p)
+
+
+def makeFolders(e1, e2, date):
+    datedir = os.path.join(DST, date)
+    gl.dstdir = os.path.join(datedir, '{0}_{1}'.format(e1, e2))
+    for d in [TEMP, DST, datedir, gl.dstdir]:
+        if not os.path.exists(d):
+            try: os.makedirs(d)
+            except: pass
+
+
+def etreeNumber(e):
+    for j in e.split('/')[-2].split(('.')):
+        try: return int(j)
+        except: pass
+
+
+def matchStart(FILE1, FILE2, TUNING, DATE, TUNING_DIFF):
+    filename1 = FILE1[0]
+    filename2 = FILE2[0]
+    
+    etree_number1 = etreeNumber(filename1)
+    etree_number2 = etreeNumber(filename2)
+
+    resfile = None
+    makeFolders(etree_number1, etree_number2, DATE)
+
+    shmname1 = '{0}_{1}_audio'.format(etree_number1, FILE1[1])
+    shm1 = shared_memory.SharedMemory(name=shmname1)
+    file1_buf = np.ndarray(FILE1[2], dtype=np.float32, buffer=shm1.buf)
+    shmname2 = '{0}_{1}_audio'.format(etree_number2, FILE2[1])
+    shm2 = shared_memory.SharedMemory(name=shmname2)
+    file2_buf = np.ndarray(FILE2[2], dtype=np.float32, buffer=shm2.buf)
+    
+    #tuning_diff = tuningDiff(file1_buf, file2_buf)
+
+    file1_resampled = resampleAudio(file1_buf, TUNING_DIFF)
+    #X = getChroma(file1_resampled, tuning_frac2)
+
+    #shmnameY = '{0}_{1}_chroma'.format(etree_number2, FILE2[1])
+    #shmY = shared_memory.SharedMemory(name=shmnameY)
+    #Y = np.ndarray(chroma_shape2, dtype=np.float32, buffer=shmY.buf)
+
+    wp_plot = match(file1_resampled, file2_buf, TUNING, TUNING_DIFF)
+
+    if len(wp_plot) > 0:
+        plotFigure(wp_plot, FILE1[2][0], FILE2[2][0], filename1, filename2, TUNING_DIFF)
+        resfile = filename1
+        dtw = wp_plot[0]
+
+    return resfile
